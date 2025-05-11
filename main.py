eeb7e154bfb20129c604c0dcd95ed7423110feba
Ahmed-Naserelden <an2071497@gmail.com>
2025-05-11 22:36:08 +0300

    refactor: simplify main pipeline execution by introducing Workflow class and removing Pipeline class

--- main.py
+++ main.py
@@ -1,66 +1,7 @@
-# In any script or module
-import sys
-import os
-import pandas as pd
-from utils import PipelineLogger
-from pipeline import FileExtractor, FileLoader
-from pipeline import CustomerTransformer, SupportTicketsTransformer
-from pipeline import BillingTransformer, LoansTransformer, TransactionsTransformer
+from .workflow import Workflow
 
-logger = PipelineLogger(__name__).get_logger()
-
-class Pipeline():
-
-    def __init__(self, input_file_path: str, output_path: str, output_file_name: str = None, output_file_format: str = 'csv'):
-        self.__input_path = input_file_path
-        self.__output_path = output_path
-        self.__partition_date = input_file_path.split('/')[-3]
-        self.__partion_hour = input_file_path.split('/')[-2]
-        self.__output_file_name = output_file_name
-        self.__output_file_format = output_file_format
-        self.__dataframe = None
-        self.__transformer_kind = input_file_path.split('/')[-1].split('.')[0]
-        self.__transformers = {
-            'customer_profiles': CustomerTransformer, 
-            'billing': BillingTransformer, 
-            'loans': LoansTransformer, 
-            'transactions': TransactionsTransformer,
-            'support_tickets': SupportTicketsTransformer
-        }
-
-    def extract(self):
-        response = FileExtractor.extract(self.__input_path)
-        if response['valid']:
-            self.__dataframe = response['data']
-
-    def transform(self):
-        transformer = self.__transformers[self.__transformer_kind](self.__partition_date, self.__partion_hour)
-        self.__dataframe = transformer.transform(self.__dataframe)
-
-    def load(self):
-        loader = FileLoader(self.__dataframe, 
-                            self.__output_path, 
-                            self.__output_file_name, 
-                            self.__output_file_format)
-        loader.load()
-    
-    def run(self):
-        self.extract()
-        self.transform()
-        self.load()
-    
-    def showDataFrame(self):
-        print(self.__dataframe.head())
-
-    def getDataFrame(self):
-        return self.__dataframe
-
-if __name__ == '__main__':
-
-    input_file_path = 'incoming_data/2025-04-18/14/loans.txt'
-    output_path = 'archive/'
-
-    print("Starting pipeline...")
-    pipeline = Pipeline(input_file_path, output_path)
-    data = pipeline.run()  
-    print("Pipeline completed successfully")
+def main():
+    workflow = Workflow()
+    workflow.create_pipeline('customer_profiles.csv')
+    workflow.create_pipeline('loans.txt')
+    workflow.run()
